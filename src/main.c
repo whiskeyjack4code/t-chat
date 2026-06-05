83a048506b785730b94a585fb1a7525952ffb887
David McMahon <davidmcmhn@gmail.com>
2026-06-05 17:02:12 +0000

    Fixed infinite loop by checking for 0 in recv()

--- src/main.c
+++ src/main.c
@@ -43,53 +43,63 @@ int main(void){
         return 1;
     }
 
-    // Make the server accept a connection
-    struct sockaddr_in client_addr;
-    memset(&client_addr, 0, sizeof(client_addr));
+    for(;;){
+        printf("Waiting for connections..\n");
+
+        // Make the server accept a connection
+        struct sockaddr_in client_addr;
+        memset(&client_addr, 0, sizeof(client_addr));
 
 
-    socklen_t client_addr_len = sizeof(client_addr);
+        socklen_t client_addr_len = sizeof(client_addr);
 
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
 
-    if(client_fd == -1){
-        perror("accept");
-        close(server_fd);
-        return 1;
-    }
+        if(client_fd == -1){
+            perror("accept");
+            close(server_fd);
+            break;
+        }
 
-    //////// Client Work /////////////////////////
-    char ip_string[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &client_addr.sin_addr, ip_string, sizeof(ip_string));
-    int client_port = ntohs(client_addr.sin_port);
+        //////// Client Work /////////////////////////
+        char ip_string[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &client_addr.sin_addr, ip_string, sizeof(ip_string));
+        int client_port = ntohs(client_addr.sin_port);
 
-    printf("Client IP: %s:%d\n", ip_string, client_port);
+        printf("Client Connected [%s:%d]\n", ip_string, client_port);
 
-    // Sending and Receiving Data
-    char recv_buffer[1024];
-    ssize_t bytes_read = recv(client_fd, recv_buffer, sizeof(recv_buffer)-1, 0);
+        for(;;){
+             // Sending and Receiving Data
+            char recv_buffer[1024];
 
-    if(bytes_read < 0){
-        perror("recv");
+            ssize_t bytes_read = recv(client_fd, recv_buffer, sizeof(recv_buffer)-1, 0);
+
+            if(bytes_read < 0){
+                perror("recv");
+                close(client_fd);
+                break;
+            }
+
+            if(bytes_read == 0){
+                printf("[%s:%d] disconnected\n", ip_string, client_port);
+                break;
+            }
+
+            recv_buffer[bytes_read] = '\0';
+            printf("Client: %s\n", recv_buffer);
+
+            char *send_buffer = recv_buffer;
+            ssize_t bytes_sent = send(client_fd, send_buffer, bytes_read, 0);
+            if(bytes_sent < 0) {
+                perror("send");
+                close(client_fd);
+                break;
+            }
+        }
+       
         close(client_fd);
-        close(server_fd);
-        return 1;
     }
 
-    recv_buffer[bytes_read] = '\0';
-    printf("Client: %s\n", recv_buffer);
-
-    char *send_buffer = recv_buffer;
-    ssize_t bytes_sent = send(client_fd, send_buffer, bytes_read, 0);
-
-    if(bytes_sent < 0) {
-        perror("send");
-        close(client_fd);
-        close(server_fd);
-        return 1;
-    }
-
-    close(client_fd);
     close(server_fd);
     
     return 0;
